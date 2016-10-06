c232138fcaa80abdc7e5c01149b054622972f51f
Thiago Paes <mrprompt@Mac-mini-de-Thiago.local>
2016-10-06 16:25:47 -0300

    refactoring code and fix decimals problem

--- js/angular-price-format.js
+++ js/angular-price-format.js
@@ -4,31 +4,23 @@ angular.module('angular-price-format', [])
         require: '?ngModel',
         link: function (scope, elem, attrs, ctrl) {
             if (!ctrl) return;
-            ctrl.$formatters.unshift(function (a) {
-                elem[0].value = parseFloat((ctrl.$modelValue * 100).toFixed((typeof attrs.decimals  === 'undefined')?2 :attrs.decimals));
-                elem.priceFormat({
-                    prefix            : (typeof attrs.prefix    === 'undefined')?'' :attrs.prefix,
-                    sufix             : (typeof attrs.sufix     === 'undefined')?'' :attrs.sufix,
-                    centsSeparator    : (typeof attrs.cents     === 'undefined')?',':attrs.cents,
-                    thousandsSeparator: (typeof attrs.thousands === 'undefined')?'.':attrs.thousands,
-                    centsLimit        : (typeof attrs.decimals  === 'undefined')?2  :attrs.decimals,
-                    limit             : (typeof attrs.limit     === 'undefined')?'' :attrs.limit,
-                    allowNegative     : (typeof attrs.negative  === 'undefined')?'' :attrs.negative,
-                    insertPlusSign    : (typeof attrs.plus      === 'undefined')?'' :attrs.plus
-                });
+            var params = {
+                prefix            : (typeof attrs.prefix    === 'undefined') ? ''  : attrs.prefix,
+                sufix             : (typeof attrs.sufix     === 'undefined') ? ''  : attrs.sufix,
+                centsSeparator    : (typeof attrs.cents     === 'undefined') ? ',' : attrs.cents,
+                thousandsSeparator: (typeof attrs.thousands === 'undefined') ? '.' : attrs.thousands,
+                centsLimit        : (typeof attrs.decimals  === 'undefined') ? 2   : Number(attrs.decimals),
+                limit             : (typeof attrs.limit     === 'undefined') ? ''  : attrs.limit,
+                allowNegative     : (typeof attrs.negative  === 'undefined') ? ''  : attrs.negative,
+                insertPlusSign    : (typeof attrs.plus      === 'undefined') ? ''  : attrs.plus
+            };
+            ctrl.$formatters.unshift(function() {
+                elem[0].value = parseFloat(ctrl.$modelValue * 100).toFixed(params.centsLimit - 2);
+                elem.priceFormat(params);
                 return elem[0].value;
             });
-            ctrl.$parsers.unshift(function (viewValue) {
-                elem.priceFormat({
-                    prefix            : (typeof attrs.prefix    === 'undefined')?'' :attrs.prefix,
-                    sufix             : (typeof attrs.sufix     === 'undefined')?'' :attrs.sufix,
-                    centsSeparator    : (typeof attrs.cents     === 'undefined')?',':attrs.cents,
-                    thousandsSeparator: (typeof attrs.thousands === 'undefined')?'.':attrs.thousands,
-                    centsLimit        : (typeof attrs.decimals  === 'undefined')?2  :attrs.decimals,
-                    limit             : (typeof attrs.limit     === 'undefined')?'' :attrs.limit,
-                    allowNegative     : (typeof attrs.negative  === 'undefined')?'' :attrs.negative,
-                    insertPlusSign    : (typeof attrs.plus      === 'undefined')?'' :attrs.plus
-                });      
+            ctrl.$parsers.unshift(function() {
+                elem.priceFormat(params);
                 return elem[0].value;
             });
         }
